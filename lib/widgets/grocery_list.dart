0da614531c89da8a2193b68927ace90f9cb39f6a
ammarabid29 <aammarawan786@gmail.com>
2023-10-01 18:14:57 +0500

    fetching data

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:shopping_list/data/categories.dart';
 import 'package:shopping_list/models/grocery_item.dart';
 import 'package:shopping_list/widgets/new_item.dart';
+import 'package:http/http.dart' as http;
 
 class GroceryList extends StatefulWidget {
   const GroceryList({super.key});
@@ -10,21 +14,47 @@ class GroceryList extends StatefulWidget {
 }
 
 class _GroceryListState extends State<GroceryList> {
-  final List<GroceryItem> _groceryItems = [];
+  List<GroceryItem> _groceryItems = [];
 
   void _addItem() async {
-    final newItem = await Navigator.of(context).push<GroceryItem>(
+    await Navigator.of(context).push<GroceryItem>(
       MaterialPageRoute(
         builder: ((context) => const NewItem()),
       ),
     );
-    if (newItem == null) {
-      return;
-    }
+    _loadItems();
+  }
 
+  void _loadItems() async {
+    final url = Uri.https(
+        'flutter-prep-55363-default-rtdb.asia-southeast1.firebasedatabase.app',
+        'shopping-list.json');
+    final response = await http.get(url);
+    final Map<String, dynamic> listData = json.decode(response.body);
+    final List<GroceryItem> loadedItems = [];
+    for (final items in listData.entries) {
+      final category = categories.entries
+          .firstWhere(
+              (catItem) => catItem.value.name == items.value['category'])
+          .value;
+      loadedItems.add(
+        GroceryItem(
+          id: items.key,
+          name: items.value['name'],
+          quantity: items.value['quantity'],
+          category: category,
+        ),
+      );
+    }
     setState(() {
-      _groceryItems.add(newItem);
+      _groceryItems = loadedItems;
     });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _loadItems();
   }
 
   void _removeItem(GroceryItem item) {
